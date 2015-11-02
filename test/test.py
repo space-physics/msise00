9a7a6807047228fe8e94bc0b708eec559b2ce98a
michael <scienceopen@users.noreply.github.com>
2015-11-01 19:09:12 -0500

    selftest

---
+++ test/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from __future__ import division,absolute_import
+from numpy.testing import assert_allclose
+from datetime import datetime
+from pytz import UTC
+from numpy import array
+#
+from msise00.runmsis import rungtd1d
+
+def test_gtd1d():
+    t=datetime(2013,3,31,12,tzinfo=UTC)
+    altkm=150.
+    glat=65; glon=-148
+    f107a=100; f107=100; ap=4; mass=48.
+    tselecopts = array([1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],float)
+
+    dens,temp = rungtd1d(t,altkm,glat,glon,f107a,f107,ap,mass,tselecopts)
+
+    assert_allclose(dens.values[0,:-1],[1.19930062e+13, 1.25792119e+16, 2.92461331e+16,
+                                 2.75184702e+15, 6.24779497e+13,1.84406768e-09,
+                                 8.54530523e+11, 8.42101896e+12])
+    assert (dens.columns.values == ['He', 'O', 'N2', 'O2', 'Ar', 'Total', 'H', 'N', 'AnomalousO']).all()
+
+    assert_allclose(temp.values[0,:],[ 848.71148682,  645.71972656])
+
+if __name__ == '__main__':
+    test_gtd1d()
