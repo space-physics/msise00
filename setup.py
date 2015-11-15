dc11fc2a43c75828139cc79c443738baa4900df2
michael <scienceopen@users.noreply.github.com>
2015-11-14 23:47:21 -0500

    setup travis prereq

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import setuptools #enables develop
 from numpy.distutils.core import setup,Extension
+import subprocess
 
 
 with open('README.rst','r') as f:
@@ -22,3 +23,8 @@ setup(name='msise00',
                     sources=['fortrancode/nrlmsise00_sub.for'],
                     f2py_options=['--quiet'])]
 	  )
+
+try:
+    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+except Exception as e:
+    print('you will need to install packages in requirements.txt  {}'.format(e))
