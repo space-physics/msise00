b6a7689442a359a95960a10f3ce645e679431399
Michael Hirsch <scienceopen@users.noreply.github.com>
2016-02-02 00:27:13 -0500

    py35

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(name='msise00',
 	  )
 
 try:
-    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.run(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
 except Exception as e:
     print('you will need to install packages in requirements.txt  {}'.format(e))
