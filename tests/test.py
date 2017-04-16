5196d318ce2aac078ed26f89fed2656387e39a0b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-16 01:41:08 -0400

    badge

--- tests/test.py
+++ tests/test.py
@@ -11,10 +11,10 @@ def test_gtd1d():
     t=datetime(2013,3,31,12,tzinfo=UTC)
     altkm=150.
     glat=65.; glon=-148.
-    f107a=100; f107=100; ap=4; mass=48.
+    f107a=100; f107=100; ap=4
     tselecopts = array([1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],float)
 
-    dens,temp = rungtd1d(t,altkm,glat,glon,f107a,f107,ap,mass,tselecopts)
+    dens,temp = rungtd1d(t,altkm,glat,glon,f107a,f107,ap,tselecopts)
 
     assert_allclose(dens.values[0,:-1],[1.19930062e+13, 1.25792119e+16, 2.92461331e+16,
                                  2.75184702e+15, 6.24779497e+13,1.84406768e-09,
