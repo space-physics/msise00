c61b2618fc22aad1b557e71f2e2fa1fca3ca9295
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-19 17:06:22 -0400

    move

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
 
 setup(ext_modules=[Extension(name='gtd7',
-                             sources=['fortran/nrlmsise00_sub.for'],
+                             sources=['src/nrlmsise00_sub.for'],
                              f2py_options=['--quiet'])])
