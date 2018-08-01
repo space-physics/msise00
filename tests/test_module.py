6a13e9721be252296407ad1a3b2d057972e4719a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-01 17:28:01 -0400

    pytest template

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from numpy.testing import assert_allclose
 from datetime import datetime
 import pytest
+from pytest import approx
 import msise00
 
 
@@ -18,19 +18,19 @@ def test_gtd1d():
     dims = list(atmos.dims)
     assert ['alt_km', 'lat', 'lon', 'time'] == dims
 
-    assert_allclose(atmos['He'], 11908118740992.0)
-    assert_allclose(atmos['O'],  1.306165589835776e+16)
-    assert_allclose(atmos['N2'], 3.051389580214272e+16)
-    assert_allclose(atmos['O2'],  2664322295660544.0)
-    assert_allclose(atmos['Ar'],  67772830711808.0)
-    assert_allclose(atmos['Total'], 1.9115256044699436e-09)
-    assert_allclose(atmos['N'],  9171036536832.0)
-    assert_allclose(atmos['AnomalousO'], 5.380620096337701e-15)
+    assert atmos['He'] == approx(11908118740992.0)
+    assert atmos['O'] == approx(1.306165589835776e+16)
+    assert atmos['N2'] == approx(3.051389580214272e+16)
+    assert atmos['O2'] == approx(2664322295660544.0)
+    assert atmos['Ar'] == approx(67772830711808.0)
+    assert atmos['Total'] == approx(1.9115256044699436e-09)
+    assert atmos['N'] == approx(9171036536832.0)
+    assert atmos['AnomalousO'] == approx(5.380620096337701e-15)
 
     assert atmos.species == ['He', 'O', 'N2', 'O2', 'Ar', 'Total', 'H', 'N', 'AnomalousO']
 
-    assert_allclose(atmos['Tn'],   681.584167)
-    assert_allclose(atmos['Texo'], 941.289246)
+    assert atmos['Tn'] == approx(681.584167)
+    assert atmos['Texo'] == approx(941.289246)
 
 
 def test_forecast():
@@ -46,20 +46,20 @@ def test_forecast():
     dims = list(atmos.dims)
     assert ['alt_km', 'lat', 'lon', 'time'] == dims
 
-    assert_allclose(atmos['He'], 4391892942848.0)
-    assert_allclose(atmos['O'],  4572213877407744.0)
-    assert_allclose(atmos['N2'], 3.699869941864858e+16)
-    assert_allclose(atmos['O2'],  8123202106556416.0)
-    assert_allclose(atmos['Ar'],  335927545167872.0)
-    assert_allclose(atmos['Total'], 2.2950290556877917e-09)
-    assert_allclose(atmos['N'],  2154773479424.0)
-    assert_allclose(atmos['AnomalousO'], 8.750423680563095e-16)
+    assert atmos['He'] == approx(4391892942848.0)
+    assert atmos['O'] == approx(4572213877407744.0)
+    assert atmos['N2'] == approx(3.699869941864858e+16)
+    assert atmos['O2'] == approx(8123202106556416.0)
+    assert atmos['Ar'] == approx(335927545167872.0)
+    assert atmos['Total'] == approx(2.2950290556877917e-09)
+    assert atmos['N'] == approx(2154773479424.0)
+    assert atmos['AnomalousO'] == approx(8.750423680563095e-16)
 
     assert atmos.species == ['He', 'O', 'N2', 'O2', 'Ar', 'Total', 'H', 'N', 'AnomalousO']
 
-    assert_allclose(atmos['Tn'],   666.9913330078125)
-    assert_allclose(atmos['Texo'], 922.104736328125)
+    assert atmos['Tn'] == approx(666.9913330078125)
+    assert atmos['Texo'] == approx(922.104736328125)
 
 
 if __name__ == '__main__':
-    pytest.main()
+    pytest.main(['-x', __file__])
--- tests/test_script.py
+++ tests/test_script.py
@@ -5,20 +5,17 @@ import subprocess
 import pytest
 import xarray
 import xarray.tests
-try:
-    import scipy
-except ImportError:
-    scipy = None
 
 R = Path(__file__).parent
 
 
-@pytest.mark.skipif(scipy is None, reason='no netcdf')
 def test_one_alt_one_time():
     """
     Regenerate ref3.nc by:
         ./msis00.py -q -w ref3.nc -a 200 -t 2017-03-01T12
     """
+    pytest.importorskip('netCDF4')
+
     with tempfile.TemporaryDirectory() as d:
         fn = Path(d) / 'test.nc'
         subprocess.check_call(['msis00', '-q', '-w', str(fn), '-a', '200', '-t', '2017-03-01T12'])
@@ -28,12 +25,13 @@ def test_one_alt_one_time():
         xarray.tests.assert_allclose(ref, dat)
 
 
-@pytest.mark.skipif(scipy is None, reason='no netcdf')
 def test_time_range():
     """
     Regenerate ref4.nc by:
         ./msis00.py -q -w ref4.nc -gs 90 90 -t 2017-03-01T12 2017-03-01T14
     """
+    pytest.importorskip('netCDF4')
+
     with tempfile.TemporaryDirectory() as d:
         fn = Path(d) / 'test.nc'
         subprocess.check_call(['msis00', '-q', '-w', str(fn),
@@ -45,12 +43,13 @@ def test_time_range():
         xarray.tests.assert_allclose(ref, dat)
 
 
-@pytest.mark.skipif(scipy is None, reason='no netcdf')
 def test_one_loc_one_time():
     """
     regenererate ref6.nc by:
         ./msis00.py -q -w ref6.nc -t 2017-03-01T12 -c 65 -148
     """
+    pytest.importorskip('netCDF4')
+
     with tempfile.TemporaryDirectory() as d:
         fn = Path(d) / 'test.nc'
         subprocess.check_call(['msis00', '-q', '-w', str(fn), '-a', '200',
@@ -61,12 +60,13 @@ def test_one_loc_one_time():
         xarray.tests.assert_allclose(ref, dat)
 
 
-@pytest.mark.skipif(scipy is None, reason='no netcdf')
 def test_one_alt_one_time_one_loc():
     """
     regenerate ref5.nc by:
         ./msis00.py -q -w ref5.nc -a 100 -t 2017-03-01T12 -c 65 -148
     """
+    pytest.importorskip('netCDF4')
+
     with tempfile.TemporaryDirectory() as d:
         fn = Path(d) / 'test.nc'
         subprocess.check_call(['msis00', '-q', '-w', str(fn),
@@ -78,4 +78,4 @@ def test_one_alt_one_time_one_loc():
 
 
 if __name__ == '__main__':
-    pytest.main()
+    pytest.main(['-x', __file__])
