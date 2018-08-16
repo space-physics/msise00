f3ff7de38f550e8feb22cf920948034d2bc102e5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-16 11:41:22 -0400

    build template

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@ import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
 
 setup(ext_modules=[Extension(name='gtd7',
-                             sources=['src/nrlmsise00_sub.for'],
-                             f2py_options=['--quiet'])])
+                             sources=['src/nrlmsise00_sub.for'])])
