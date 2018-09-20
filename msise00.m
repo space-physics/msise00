a083292da453d48348a9cb88535857c9cf1e4860
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-20 11:56:27 -0400

    formatting [ci skip]

--- msise00.m
+++ msise00.m
@@ -59,7 +59,7 @@ function I = xarrayind2vector(V, key)
 C = cell(V.indexes{key}.values.tolist);  % might be numeric or cell array of strings
 
 if iscellstr(C) || (length(C) > 1 && isa(C{1}, 'py.str'))
-    I = cellfun(@char,C, 'uniformoutput',false);
+    I = cellfun(@char, C, 'uniformoutput', false);
 elseif isa(C{1}, 'py.datetime.datetime')
     I = char(C{1}.isoformat());
 else
