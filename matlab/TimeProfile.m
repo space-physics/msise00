60964caa1e6b154605c256c43a79aada486c7688
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-03 23:24:16 -0500

    add time profile Matlab [skip ci]

---
+++ matlab/TimeProfile.m
@@ -0,0 +1,17 @@
+% geographic WGS84 lat,lon,alt
+glat = 65.1;
+glon = -147.5;
+f107a = 150;
+f107 = 150;
+Ap = 4;
+alt_km = 400;
+t0 = '2015-12-13 10:00:00';
+t1 = '2015-12-14 10:00:00';
+
+time = datetimerange(t0, t1, 3600);
+
+for i = 1:length(time)
+    atmos(i) = msise00(time(i), glat, glon, f107a, f107, Ap, alt_km); %#ok<SAGROW>
+end
+
+plottime(atmos, time, glat, glon)
---
+++ matlab/plottime.m
@@ -0,0 +1,28 @@
+function plottime(atmos, times, glat, glon)
+
+figure(1); clf(1)
+ax1 = subplot(2,1,1);
+set(ax1,'nextplot','add')
+
+semilogy(ax1,times, [atmos.nN2], 'DisplayName','N_{N_2}')
+semilogy(ax1,times, [atmos.nO2], 'DisplayName','N_{O_2}')
+
+
+datetick(ax1,'x',13)
+title(ax1,[datestr(times(1), 29), ' (',num2str(glat),',',num2str(glon),')'])
+ylabel(ax1,'m^{-3}')
+
+legend(ax1,'show','location','southwest')
+
+
+%% smaller quantities
+ax = subplot(2,1,2);
+set(ax,'nextplot','add')
+semilogy(ax,times, [atmos.nAr], 'DisplayName','N_{Ar}')
+
+datetick(ax,'x',13)
+ylabel(ax,'m^{-3}')
+xlabel(ax, 'time [UTC]')
+
+legend(ax,'show')
+end
