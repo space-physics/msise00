0f510b95f57e71dad08e41a0e2e6b931401975fe
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-16 11:43:59 -0400

    matlab:cmake apply macos path workaround

--- +msise00/private/cmake.m
+++ +msise00/private/cmake.m
@@ -5,20 +5,46 @@ arguments
   src_dir (1,1) string
 end
 
-ccmd = "ctest -S " + fullfile(src_dir, "setup.cmake") +  " -VV";
+fix_macos()
 
-ret = system('cmake --version');
+cmd = "cmake --version";
+ret = system(cmd);
 if ret ~= 0
-  error('cmake:environment_error', 'CMake not found')
+  error('cmake:runtime_error', 'CMake not found')
 end
+
+cmd = "ctest -S " + fullfile(src_dir, "setup.cmake") +  " -VV";
+
 
 if ~isfolder(src_dir)
   error("cmake:file_not_found", "source directory not found: " + src_dir)
 end
 
-ret = system(ccmd);
+ret = system(cmd);
 if ret ~= 0
   error('cmake:runtime_error', 'error building with CMake')
 end
 
 end
+
+
+function fix_macos()
+
+%% MacOS PATH workaround
+% Matlab does not seem to load .zshrc or otherwise pickup shell "export" like
+% Matlab on Linux or Windows does, so we apply these MacOS-specific workaround
+if ~ismac
+  return
+end
+
+sys_path = getenv("PATH");
+needed_paths = "/usr/local/bin";
+for np = needed_paths
+  if isfolder(np) && ~contains(sys_path, np)
+    sys_path = np + pathsep + sys_path;
+  end
+end
+
+setenv('PATH', sys_path)
+
+end % function
