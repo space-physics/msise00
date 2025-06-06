bdae01980d7f48b822be000b3442f376d717494e
scivision <scivision@users.noreply.github.com>
2025-06-06 13:28:24 -0400

    test that geomagindices match

--- src/msise00/tests/test_point.py
+++ src/msise00/tests/test_point.py
@@ -9,13 +9,18 @@ import importlib.resources
 import subprocess
 import pytest
 import sys
+from datetime import datetime
+
 import xarray
 import xarray.testing
+
+
+import geomagindices as gi
 import msise00
 
 lat = 65.0
 lon = -148.0
-time = "2017-03-01T12"
+time = datetime(2017, 3, 1, 12)
 
 
 @pytest.mark.parametrize("altkm,reffn", [(100.0, "ref5.nc"), (200.0, "ref6.nc")])
@@ -24,13 +29,22 @@ def test_one_loc_one_time(altkm, reffn):
         ref = xarray.open_dataset(fn)
 
     try:
-        dat = msise00.run(time, altkm, lat, lon).squeeze()
+        i = gi.get_indices(time, smoothdays=81).squeeze().to_dict()
     except ConnectionError:
         pytest.skip("unable to download RecentIndices.txt")
+
+    assert i["Ap"] == 39
+    assert i["Kp"] == pytest.approx(4.7)
+    assert i["f107"] == pytest.approx(79.3)
+    assert i["f107s"] == pytest.approx(74.989727)
+    assert i["Aps"] == pytest.approx(9.675052)
+
+    dat = msise00.run(time, altkm, lat, lon, indices=i).squeeze()
 
     assert dat.lat == ref.lat == lat
     assert dat.lon == ref.lon == lon
     assert dat.time == ref.time
+
     xarray.testing.assert_allclose(ref, dat, rtol=0.001)
 
 
